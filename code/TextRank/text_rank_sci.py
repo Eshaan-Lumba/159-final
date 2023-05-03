990ce67e81051170abf3474b95964b535381044d
claireleblanc <67252968+claireleblanc@users.noreply.github.com>
2023-05-02 21:34:38 -0700

    Update and rename text_rank2.py to text_rank_sci.py

--- code/TextRank/text_rank_sci.py
+++ code/TextRank/text_rank_sci.py
@@ -12,9 +12,9 @@ def text_rank(sum_file, stop_file):
     threshold = 1.3
     total = 0
 
-    sumfile = open(sum_file,"r+", encoding="utf8")
+    sumfile = open(sum_file,"r+", encoding="utf8").readlines()
     for line in sumfile: 
-        if total > 50:
+        if total >= 50:
             break
         term_count = {}
         tf_table = {}
@@ -23,14 +23,11 @@ def text_rank(sum_file, stop_file):
         idf_table = {}
         scores = {}
 
-        data = json.loads(line)
         stopfile = open(stop_file,"r+", encoding="utf8")
         stop = [w.replace('\n','').lower() for w in stopfile.readlines()]
 
-        text = data['text']
+        text = line
         text = text.replace('\n',' ')
-
-        #text='''Santiago is a Shepherd who has a recurring dream which is supposedly prophetic. Inspired on learning this, he undertakes a journey to Egypt to discover the meaning of life and fulfill his destiny. During the course of his travels, he learns of his true purpose and meets many characters, including an “Alchemist”, that teach him valuable lessons about achieving his dreams. Santiago sets his sights on obtaining a certain kind of “treasure” for which he travels to Egypt. The key message is, “when you want something, all the universe conspires in helping you to achieve it.” Towards the final arc, Santiago gets robbed by bandits who end up revealing that the “treasure” he was looking for is buried in the place where his journey began. The end.'''
 
         sentences=text.split(". ")
         N = len(sentences)
@@ -46,7 +43,6 @@ def text_rank(sum_file, stop_file):
             term_count[sent_str] = {}
             seen = set()
 
-            #Count
             for word in sentence:
                 word = ps.stem(word)
                 term_count[sent_str][word] = term_count[sent_str].get(word,0) + 1
@@ -59,26 +55,12 @@ def text_rank(sum_file, stop_file):
             idf_table[word] = {}
             idf_table[word] = math.log(N/doc_freq[word],10)
 
-        
-
         #calculate term frequency
         for sent, count_table in term_count.items():
             tf_idf[sent] = {}
             total_count = len(count_table)
             for word, count in count_table.items():
                 tf_idf[sent][word] = count / total_count * idf_table[word]
-            
-
-        #create tf_idf matrix
-        # for sent, count_table in term_count.items():
-        #     tf_idf[sent] = {}
-        #     for word, tf in count_table.items():
-        #         tf_idf[sent][word] = tf * idf_table[word]
-
-        #Score sentences
-        #Add up TF-IDF and divide by total number of words
-
-
 
         for sent, count_table in tf_idf.items(): 
             if len(count_table) == 0:
